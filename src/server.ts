1af992cd432b077fee671d6924d170b1bff265d0
mactav1sh <mactavish104@gmail.com>
2023-02-06 14:01:01 +0200

    updated server starting method

--- src/server.ts
+++ src/server.ts
@@ -8,10 +8,9 @@ const db = process.env.DATABASE_URL as string;
 
 mongoose.connect(db).then(() => {
   console.log('database connected');
+  app.listen(port, () => {
+    console.log('server started');
+  });
 });
 
 const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-  console.log('started');
-});
